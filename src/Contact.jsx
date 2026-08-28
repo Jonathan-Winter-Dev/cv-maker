69bac94786c09c6a01afccecd5fcc66b098923be
Jonathan Winter <jonathan.winter@hotmail.com>
2026-08-28 14:46:31 +0100

    contact form has more fields

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,30 +1,37 @@
 import { useState } from "react";
 
 export default function Contact() {
-  const [firstName, setFirstname] = useState("");
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+    phone: "",
+  });
 
   function handleSubmit(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
     const dataObject = Object.fromEntries(formData);
-    setFirstname(dataObject.fname);
-    console.log(dataObject);
-  }
 
-  function handleChange(e) {
-    setFirstname(e.target.value);
+    setContact({
+      fName: dataObject.fName,
+      lName: dataObject.lName,
+      email: dataObject.email,
+      phone: dataObject.phone,
+    });
   }
 
   function ContactForm() {
     return (
       <form onSubmit={handleSubmit}>
-        <label for="fname">First Name</label>
-        <input
-          type="text"
-          name="fname"
-          id="fname"
-          // onChange={handleChange}
-        />
+        <label for="fName">First Name:</label>
+        <input type="text" name="fName" id="fName" required />
+        <label for="fName">Last Name:</label>
+        <input type="text" name="lName" id="lName" required />
+        <label for="fName">Email:</label>
+        <input type="email" name="email" id="email" />
+        <label for="fName">Phone Number:</label>
+        <input type="tel" name="phone" id="phone" />
         <input type="submit" />
       </form>
     );
@@ -33,7 +40,10 @@ export default function Contact() {
   return (
     <>
       <ContactForm />
-      <p>{firstName}</p>
+      <p>{contact.fName}</p>
+      <p>{contact.lName}</p>
+      <p>{contact.email}</p>
+      <p>{contact.phone}</p>
     </>
   );
 }
