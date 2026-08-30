848f445e9f2e5ce1030ba5d910e983137c5f0693
Jonathan Winter <jonathan.winter@hotmail.com>
2026-08-30 13:27:40 +0100

    add delete job button

--- src/employment/Employment.jsx
+++ src/employment/Employment.jsx
@@ -9,12 +9,16 @@ export default function Employment() {
   const jobDisplay = jobsArr.map((item) => {
     return (
       <div className="jobContainer" key={item.id}>
-        <JobForm updateJob={updateJob} job={item} />
+        <JobForm updateJob={updateJob} deleteJob={deleteJob} job={item} />
         <p>{item.employer}</p>
         <p>{item.title}</p>
       </div>
     );
   });
+
+  function deleteJob(id) {
+    setJobs(jobs.filter((item) => item.id !== id));
+  }
 
   function addJob() {
     const jobArr = [...jobs];
