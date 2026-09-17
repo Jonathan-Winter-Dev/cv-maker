ca0d388dfd6531bff8aac7b103ab428d5f26855a
Jonathan Winter <jonathan.winter@hotmail.com>
2026-09-17 20:05:54 +0100

    add accordion animation

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -3,16 +3,21 @@ import { useState } from "react";
 export default function Accordion({ title, content }) {
   const [isActive, setIsActive] = useState(false);
 
-  function handleUpdateActive() {
+  function handleUpdateActive(e) {
     setIsActive(!isActive);
+    const parent = e.target.parentElement;
+    parent.classList.toggle("accordionVisible");
+
+    parent.querySelector(".accordionContent").classList.toggle("visible");
   }
   return (
-    <div className="accordion">
+    <div className={`accordion ${isActive ? "accordionVisible" : ""}`}>
       <div className="accordionTitle" onClick={handleUpdateActive}>
         <h1>{title}</h1>
         <div>{isActive ? "-" : "+"}</div>
       </div>
-      {isActive && <div className="accordionContent">{content}</div>}
+      {/* {isActive && <div className="accordionContent">{content}</div>} */}
+      <div className="accordionContent">{content}</div>
     </div>
   );
 }
