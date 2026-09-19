8071b4b3637d6fd0f85d957a4af512e2a3d9ff2b
Jonathan Winter <jonathan.winter@hotmail.com>
2026-09-19 10:01:07 +0100

    add symbols to accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -15,9 +15,43 @@ export default function Accordion({ title, content }) {
     <div className={`accordion ${isActive ? "accordionVisible" : ""}`}>
       <div className="accordionTitle" onClick={handleUpdateActive}>
         <h1>{title}</h1>
-        <div>{isActive ? "-" : "+"}</div>
+        <div className="accordionVisibilitySymbol">
+          <svg
+            className={`verticalBar ${isActive ? "accordionVisible" : ""}`}
+            xmlns="http://w3.org"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <rect
+              x="11"
+              y="2"
+              width="2"
+              height="20"
+              rx="1"
+              ry="1"
+              fill="currentColor"
+            />
+          </svg>
+          <svg
+            className="horizontalBar"
+            xmlns="http://w3.org"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <rect
+              x="11"
+              y="2"
+              width="2"
+              height="20"
+              rx="1"
+              ry="1"
+              fill="currentColor"
+            />
+          </svg>
+        </div>
       </div>
-      {/* {isActive && <div className="accordionContent">{content}</div>} */}
       <div className="accordionContent">{content}</div>
     </div>
   );
